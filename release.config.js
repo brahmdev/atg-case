8cfe1e8e2b008b6135017b546543f468fb67809f
Brahmdev.Pandey <brahmdev.pandey@smartbear.com>
2023-05-26 10:25:18 +0200

    chore: add initial configuration

---
+++ release.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  branches: [
+    "main",
+  ],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "CHANGELOG.md"
+      }
+    ],
+    ["@semantic-release/npm", {
+      "npmPublish": false
+    }],
+    "@semantic-release/github",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md", "package.json", "dist/**"],
+        message: "chore(release): set `package.json` to ${nextRelease.version}"
+      }
+    ]
+  ]
+};
+
